699f69202e0bbc760e0e8fac2d0d1c1420441f91
qfsly <ycqfsly@163.com>
2015-07-17 14:04:04 +0800

    戳游戏

---
+++ lottery/src/app/Game/JabGameBase.lua
@@ -0,0 +1,13 @@
+--
+-- Author: Yangcao
+-- Date: 2015-07-03 09:26:17
+--
+local JabGameBase = class("JabGameBase", function ()
+	return GameBase.new()
+end)
+
+function JabGameBase:ctor()
+	
+end
+
+return JabGameBase
