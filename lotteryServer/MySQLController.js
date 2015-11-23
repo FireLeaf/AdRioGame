2c88f5f5b6ef4bac003ecf71afe5de3cdea6a1ac
root <root@iZ253yunhieZ.(none)>
2015-11-23 21:33:29 +0800

    同步服务器

--- lotteryServer/MySQLController.js
+++ lotteryServer/MySQLController.js
@@ -19,7 +19,7 @@ MySQLController.prototype.connect = function(server, user_name, passwd){
 };
 
 MySQLController.prototype.disconnect = function(){
-    if ( is_connected() ){
+    if ( this.is_connected() ){
         this.connection.end(function(err){
             console.warn(err);
         });
@@ -27,14 +27,14 @@ MySQLController.prototype.disconnect = function(){
 }
 
 MySQLController.prototype.is_connected = function(){
-    return (this.connection != null) && this.connection.connect(function(err){
+    return (this.connection != null) ;/*&& this.connection.connect(function(err){
             console.warn(err);
-        });
+        });*/
 }
 
 //callback func(error, reult)
 MySQLController.prototype.query = function(queryInfo, callback){
-    if ( is_connected() ){
+    if ( this.is_connected() ){
         this.connection.query(queryInfo, callback);
     }
     else {
